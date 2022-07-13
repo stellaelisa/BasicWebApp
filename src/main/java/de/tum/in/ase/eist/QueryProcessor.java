ee7ac714c94b08ab90d5c70099fb7cf4ea03e5fb
stella <stella.koschnike@tum.de>
2022-07-13 11:33:45 +0200

    rechnung

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,8 +12,17 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
-        } else { // TODO extend the programm here
+           return "stella";
+        } else if (query.contains("what")) {
+            return "7";
+        }
+
+
+
+        else
+        { // TODO extend the programm here
+
+
             return "";
         }
     }
